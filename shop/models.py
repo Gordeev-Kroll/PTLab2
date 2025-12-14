e49d966e55e692c3070145d452572c88ac67b5d5
Artem Gordeev <artemgord2903@gmail.com>
2025-12-14 17:08:56 +0300

    Implementation of the logic of using promo codes

--- shop/models.py
+++ shop/models.py
@@ -1,12 +1,28 @@
 from django.db import models
 
+
 # Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.PositiveIntegerField()
+    promo_code = models.ForeignKey(
+        "PromoCode", null=True, blank=True, on_delete=models.SET_NULL
+    )
+
+    def __str__(self):
+        return self.name
+
 
 class Purchase(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     person = models.CharField(max_length=200)
     address = models.CharField(max_length=200)
     date = models.DateTimeField(auto_now_add=True)
+
+
+class PromoCode(models.Model):
+    code = models.CharField(max_length=20, unique=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.code
--- shop/views.py
+++ shop/views.py
@@ -1,21 +1,42 @@
-from django.shortcuts import render
+from django.db.models import Q
 from django.http import HttpResponse
+from django.shortcuts import redirect, render
 from django.views.generic.edit import CreateView
 
-from .models import Product, Purchase
+from shop.forms import PromoCodeForm
+
+from .models import Product, PromoCode, Purchase
+
 
 # Create your views here.
 def index(request):
-    products = Product.objects.all()
-    context = {'products': products}
-    return render(request, 'shop/index.html', context)
+    entered_code = request.session.get("promo_code")
+    if entered_code and PromoCode.objects.filter(code=entered_code).exists():
+        products = Product.objects.filter(
+            Q(promo_code__isnull=True) | Q(promo_code__code=entered_code)
+        )
+    else:
+        products = Product.objects.filter(promo_code__isnull=True)
+
+    form = PromoCodeForm()
+    context = {"products": products, "form": form}
+    return render(request, "shop/index.html", context)
 
 
 class PurchaseCreate(CreateView):
     model = Purchase
-    fields = ['product', 'person', 'address']
+    fields = ["product", "person", "address"]
 
     def form_valid(self, form):
         self.object = form.save()
-        return HttpResponse(f'Спасибо за покупку, {self.object.person}!')
+        return HttpResponse(f"Спасибо за покупку, {self.object.person}!")
 
+
+def apply_promo(request):
+    if request.method == "POST":
+        form = PromoCodeForm(request.POST)
+        if form.is_valid():
+            code = form.cleaned_data["code"]
+            if PromoCode.objects.filter(code=code).exists():
+                request.session["promo_code"] = code
+    return redirect("index")
